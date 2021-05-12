89ea4fb895fcdbd1921a34a22cbf9fe22d6c3c06
tpouhuk <me@example.com>
2021-05-12 14:15:15 +0300

    Add tests

---
+++ tests/test_physics.py
@@ -0,0 +1,18 @@
+import viroro.physics as ph
+import pymunk
+from pymunk import Vec2d
+
+
+def test_drawoptions():
+    do = ph.DrawOptions(canvas=None, zoom=1)
+
+    do.scale_screen(Vec2d.zero())
+    do.to_screen(Vec2d.zero())
+
+
+def test_distance_sensor():
+    ph.DistanceSensor(
+            pymunk.Body(),
+            x=0, y=0, angle=0, range_=0, 
+            beam_width=0, shape_filter=pymunk.ShapeFilter())
+
